02bf4309c31af8dbdbd00cf4988a5678b7d960fd
nihargoyal <goyalnihar786@gmail.com>
2023-05-06 14:51:20 +0000

    allowing hosts and debug menu

--- calenderAuthapi/settings.py
+++ calenderAuthapi/settings.py
@@ -22,9 +22,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-_tn9zbbpi#qtj_c3tc64mr3w96lx$*t2*7wakcw30ayf_nua3*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
